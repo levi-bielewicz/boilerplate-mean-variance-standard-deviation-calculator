ce1c8fa4ff8a9545a021fa5c558fae4458d061d0
levi-bielewicz <156845886+levi-bielewicz@users.noreply.github.com>
2024-01-19 11:51:58 -0500

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -7,8 +7,12 @@ def calculate(list):
     a = np.array(list).reshape(3,3)
     print(a)
 
-    
+    dictionary = dict()
+    dictionary['mean'] = [a.mean(0), a.mean(1), a.mean()]
+    dictionary['variance'] = [a.var(0), a.var(1), a.var()]
+    dictionary['standard deviation'] = [a.std(0), a.std(1), a.std()]
+    dictionary['max'] = [a.max(0), a.max(1), a.max()]
+    dictionary['min'] = [a.min(0), a.min(1), a.min()]
+    dictionary['sum'] = [a.sum(0), a.sum(1), a.sum()]
 
-
-
-    return calculations
+    return calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]
