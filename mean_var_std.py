1345a02b1f2f3d2fe1f28278472acfffa333ead2
levi-bielewicz <156845886+levi-bielewicz@users.noreply.github.com>
2024-01-19 11:55:33 -0500

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -15,4 +15,4 @@ def calculate(list):
     dictionary['min'] = [a.min(0), a.min(1), a.min()]
     dictionary['sum'] = [a.sum(0), a.sum(1), a.sum()]
 
-    return calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]
+    return calculate[(0, 1, 2, 3, 4, 5, 6, 7, 8)]
