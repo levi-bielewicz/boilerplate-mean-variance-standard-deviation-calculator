39870018056d93da851a52fe85486b646d3e21f7
levi-bielewicz <156845886+levi-bielewicz@users.noreply.github.com>
2024-01-19 11:46:40 -0500

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
 
+    a = np.array(list).reshape(3,3)
+    print(a)
+
+    
 
 
 
